581c53662750f04b974e289c7ee83762da64177a
BoonDoPark <76871728+BoonDoPark@users.noreply.github.com>
2024-10-30 17:10:25 +0900

    [refactor] 다중 사용자와 채팅 및 로그인 기능 구현

--- src/talk/talk.gateway.ts
+++ src/talk/talk.gateway.ts
@@ -1,9 +1,17 @@
-import { Logger } from '@nestjs/common';
-import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Logger, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { 
+  MessageBody, 
+  OnGatewayConnection, 
+  OnGatewayDisconnect, 
+  OnGatewayInit, 
+  SubscribeMessage, 
+  WebSocketGateway, 
+  WebSocketServer 
+} from '@nestjs/websockets';
 import { createClient } from 'redis';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway(3000, {
+@WebSocketGateway(4000, {
   namespace: 'talk',
   cors: { origin: '*' }
 })
@@ -11,24 +19,38 @@ export class TalkGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
   private redisClient = createClient({ url: 'redis://localhost:6379' });
   private logger: Logger = new Logger('TalkGateway');
 
+  @WebSocketServer() 
+  server: Server;
+
   constructor() {
     // Redis 클라이언트 연결
     this.redisClient.connect().catch(err => this.logger.error(`Failed to connect to Redis: ${err}`));
   }
 
-  @WebSocketServer() server: Server;
+  private connectedClients = [];
+  private rooms: { [key: string]: any[] } = {};
 
-  handleDisconnect(client: Socket) {
+  async handleConnection(client: Socket): Promise<void> {
     try {
-      this.logger.log('disconnection socket')
+      await this.logger.log('connection socket');
+      const socketId = client.id;
+      this.addClient(socketId);
     } catch(error) {
       throw new Error(error);
     }
   }
 
-  handleConnection(client: Socket, ...args: any[]) {
+  async handleDisconnect(client: Socket): Promise<void> {
     try {
-      this.logger.log('connection socket')
+      this.logger.log('disconnection socket');
+      this.removeClient(client.id);
+      const clientInfo = await JSON.parse(await this.redisClient.get(client.id));
+      if (clientInfo != undefined) {
+        await this.redisClient.del(client.id);
+        const clientId = clientInfo['id'];
+        const result = { status: 'off_line', id: clientId}
+        this.server.emit('client_info', result);
+      }
     } catch(error) {
       throw new Error(error);
     }
@@ -42,26 +64,78 @@ export class TalkGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
     }
   }
 
-  @SubscribeMessage('joinRoom')
-  async handleJoinRoom(@MessageBody() roomId: number, client: Socket): Promise<void> {
-    this.logger.log(client, roomId);
-    // 클라이언트를 특정 채팅방에 가입
-    client.join(`${roomId}`);
+  @SubscribeMessage('login')
+  async loginClient(client: Socket, clientId: string): Promise<void> {
+    if (clientId == '' && clientId == undefined)
+      throw new UnauthorizedException();
+    await this.redisClient.set(client.id, JSON.stringify({ id: clientId, status: 'online' }));
+    const clientInfo = await JSON.parse(await this.redisClient.get(client.id));
+    const result = { status: clientInfo.status, id: clientId };
+    this.server.emit('client_info', result);
+  }
 
-    // Redis에서 해당 채팅방의 모든 메시지를 가져와 클라이언트에 전송
-    this.redisClient.lRange(`messages:${roomId}`, 0, -1)
-      .then(messages => {
-        messages.forEach(message => {
-          client.emit('messages', message);
-        });
-      })
-      .catch(err => this.logger.error(`Failed to fetch messages from Redis for room ${roomId}: ${err}`));
+  @SubscribeMessage('logout')
+  async logoutClient(client: Socket, id: string): Promise<void> {
+    if (id == '' && id == undefined)
+      throw new NotFoundException();
+    await this.redisClient.del(client.id);
+    const result = { status: 'off_line', id};
+    this.server.emit('client_info', result);
+  }
+
+  @SubscribeMessage('updateStatus')
+  async updateStatus(client: Socket, data: any): Promise<void> {
+    if(!data)
+      throw new NotFoundException();
+    const clientInfo = await JSON.parse(await this.redisClient.get(client.id));
+    if (clientInfo != undefined) {
+      const status = data['status'];
+      const updateInfo = await JSON.parse(await this.redisClient.get(client.id));
+      if (clientInfo) {
+        updateInfo.status = status;
+        await this.redisClient.set(client.id, JSON.stringify(updateInfo.status));
+      }
+      const updateCleintInfo = await JSON.parse(await this.redisClient.get(client.id));
+      this.server.emit('update_info', updateCleintInfo);
+    }
+  }
+
+  @SubscribeMessage('joinRoom')
+  async handleJoinRoom(client: Socket, room: string): Promise<void> {
+    if (!this.rooms[room]) {
+      this.rooms[room] = [];
+    }
+    this.rooms[room].push(client.id);
+    client.join(room);
+    client.emit("join_room", "방에 입장하셨습니다.");
   }
   
-  @SubscribeMessage('messages')
-  async handleMessage(@MessageBody() { roomId, data }: { roomId: number; data: string }, client: Socket): Promise<void> {
-    this.logger.log(`Received message: ${data}`);
-    await this.redisClient.rPush(`messages:${roomId}`, data);
-    this.server.to(`${roomId}`).emit('messages', data);
+  @SubscribeMessage('sendMessage')
+  async handleMessage(client: Socket, data: { room: string; message: string }): Promise<void> {
+    const { room, message } = data;
+    this.server.to(room).emit('receive_message', {
+      message: message,
+      sender: client.id
+    });
+  }
+
+  @SubscribeMessage('leaveRoom')
+  async leaveRoom(client: Socket, room: string): Promise<void> {
+    if(this.rooms[room]) {
+      this.rooms[room] = this.rooms[room].filter(id => id !== client.id);
+    }
+    client.leave(room);
+    client.emit('leave_room', '방에서 나갔습니다.');
+  }
+
+  addClient(client: any) {
+    this.connectedClients.push(client);
+  }
+
+  removeClient(client: any) {
+    const index = this.connectedClients.indexOf(client);
+    if (index !== -1) {
+      this.connectedClients.splice(index, 1);
+    }
   }
 }
